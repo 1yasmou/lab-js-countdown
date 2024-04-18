de382272903e2bc0d27b1c0948984e8bd4c40421
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-04-18 16:14:31 +0200

    "labtimesolved"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
-let timer = null; // Variable to store the interval
+let timer = null; // Variable to store the interval 
 
 
 
@@ -9,15 +9,35 @@ let timer = null; // Variable to store the interval
 // Your code goes here ...
 
 
+const startB=document.querySelector("#start-btn");
+startB.addEventListener( "click",()=>{
+startCountdown();
+});
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
+startB.disabled=true;
+timer=setInterval(function(){
+remainingTime=remainingTime-1;
 
+document.querySelector('#time').textContent = remainingTime;
 
-  // Your code goes here ...
+//document.getElementById('time').textContent=remainingTime;
+
+if(remainingTime<=0)  {
+
+  clearInterval(timer);
+  showToast("Lift off!");
+  startB.disabled=false;
 }
+
+},1000)
+
+}
+  // Your code goes here ...
+
 
 
 
@@ -25,6 +45,42 @@ function startCountdown() {
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
+/*
+  let toast = document.querySelector('#toast');
+  //let toastMessage = document.querySelector('#toast-message');
+  //toastMessage.textContent=message;
+  //toast.style.display = 'block';
+
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove ('show');
+   },3000)
+
+   */
+
+  
+  let toast = document.querySelector('#toast');
+  let toastMessage = document.querySelector('#toast-message');
+  
+   toastMessage.textContent = message;
+ 
+
+   toast.style.visibility="visible";
+   console.log(toast);
+
+   setTimeout(() => {
+    
+    //toast.classList.remove ('show');
+    toast.style.visibility="hidden";
+
+   },3000)
+
+ 
+  // document.ge  tElementById("close-toast").onclick = function() {
+   
+    // toast.style.display = 'none'; 
+   //};
 
   // Your code goes here ...
 
